a0da840ac57586adad9d21768bdb5fb438ae8ba4
Brian Zhao <brian@bex.com.au>
2012-09-10 15:03:55 +1000

    Remove dhtmlx and add another app tweets

---
+++ tweets/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import patterns, url, include
+
+from .views import IndexView, DetailView
+
+urlpatterns = patterns('',
+    url(r'^$',
+        IndexView.as_view(),
+        name='index'),
+
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(),
+        name="detail"),
+
+)
+
